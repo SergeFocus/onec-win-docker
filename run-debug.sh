340f883e8ae6fce43726956a3af33d892220efc1
Бессонов Евгений <evgen@ievgen.ru>
2018-05-11 11:51:13 +0300

    Скрипт запуска контейнера с powershell

--- run-debug.sh
+++ run-debug.sh
@@ -1,6 +1,8 @@
 winpty docker run -it --rm \
+    --entrypoint powershell \
     -p 1540:1540 \
-    onec-srv \
-    powershell
+    -p 1541:1541 \
+    -p 1560-1591:1560-1591 \
+    onec-srv
     # -v C:\\Users\\ebessonov\\Documents\\repo\\gitlab\\onecdocker\\onec-win-docker\\distr:c:\\distr \
     # onec-srv 
